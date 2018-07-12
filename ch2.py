2fb7666b5a6a3451d47a833358ec3487290d76a6
Jocelyn Belden <jocebelden@gmail.com>
2018-07-11 23:27:05 -1000

    write first function

---
+++ ch2.py
@@ -0,0 +1,17 @@
+name = "Jocelyn"
+height_inches = 62
+height_centimeters = 157.48
+
+print("name: ", name)
+print("height_inches: ", height_inches)
+print("height_centimeters: ", height_centimeters)
+
+# This is my first function called name. It will ask the name and print a message
+
+
+def name():
+    first_name = input("What is your first name? ")
+    print('So nice to meet you, ' + first_name)
+
+
+name()
